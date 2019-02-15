b55f0ce38743bfebc0ac456a9b80de41119b44c9
HyeongJu Yoo <zerofunc@naver.com>
2019-02-16 01:41:05 +0900

    Create item43

---
+++ src/main/java/Chapter7/item43/G1.java
@@ -0,0 +1,12 @@
+package Chapter7.item43;
+
+interface G1 {
+    <E extends Exception>Object m() throws E;
+}
+
+interface G2 {
+    <F extends Exception> String m() throws Exception;
+}
+
+interface G extends G1, G2 {
+}
