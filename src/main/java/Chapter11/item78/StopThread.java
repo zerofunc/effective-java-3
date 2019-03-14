0b110a7343e6b847e3a2a5828321a572b7dad83e
HyeongJu Yoo <zerofunc@naver.com>
2019-03-15 02:13:25 +0900

    modify StopThread add sync method

--- src/main/java/Chapter11/item78/StopThread.java
+++ src/main/java/Chapter11/item78/StopThread.java
@@ -4,17 +4,24 @@ import java.util.concurrent.TimeUnit;
 
 public class StopThread {
 	private static boolean stopRequested;
-	
+
+	private static synchronized void requestStop() {
+		stopRequested = true;
+	}
+
+	private static synchronized boolean stopRequested() {
+		return stopRequested;
+	}
 	public static void main(String[] args) throws InterruptedException {
 		Thread backgroundThread = new Thread(() -> {
 			int i = 0;
-			while (!stopRequested) {
-				++i;
+			while (!stopRequested()) {
+				i++;
 			}
 		});
 		backgroundThread.start();
 		
 		TimeUnit.SECONDS.sleep(1);
-		stopRequested = true;
+		requestStop();
 	}
 }
