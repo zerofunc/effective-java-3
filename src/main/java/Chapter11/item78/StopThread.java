99a60f8dcf47af02a9ed10e3d22cdca277aaa333
HyeongJu Yoo <zerofunc@naver.com>
2019-03-15 02:16:46 +0900

    modify StopThread stopRequest volatile field

--- src/main/java/Chapter11/item78/StopThread.java
+++ src/main/java/Chapter11/item78/StopThread.java
@@ -3,25 +3,18 @@ package Chapter11.item78;
 import java.util.concurrent.TimeUnit;
 
 public class StopThread {
-	private static boolean stopRequested;
+	private static volatile boolean stopRequested;
 
-	private static synchronized void requestStop() {
-		stopRequested = true;
-	}
-
-	private static synchronized boolean stopRequested() {
-		return stopRequested;
-	}
 	public static void main(String[] args) throws InterruptedException {
 		Thread backgroundThread = new Thread(() -> {
 			int i = 0;
-			while (!stopRequested()) {
+			while (!stopRequested) {
 				i++;
 			}
 		});
 		backgroundThread.start();
 		
 		TimeUnit.SECONDS.sleep(1);
-		requestStop();
+			stopRequested = true;
 	}
 }
